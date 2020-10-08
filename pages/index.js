44f4994661662a63cc7ce4866371a29fa750fc4c
semileeBCIT <55604874+semileeBCIT@users.noreply.github.com>
2020-10-08 00:28:22 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,28 @@
-import Header from '../comps/Header';
+import React, {useState} from 'react';
+import Menu from '../comps/Menu';
+import Content from '../comps/Content';
 
 export default function Home() {
-  return (
-    <div className="main">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="contents">
-        <div>
-          <div>Content Header</div>
-          <div>Content Description</div>
-        </div>
-        <div>
-          <div>Content Header</div>
-          <div>Content Description</div>
-        </div>
-        <div>
-          <div>Content Header</div>
-          <div>Content Description</div>
-        </div>
-        <div>
-          <div>Content Header</div>
-          <div>Content Description</div>
-        </div>
-        <div>
-          <div>Content Header</div>
-          <div>Content Description</div>
-        </div>
-        <div>
-          <div>Content Header</div>
-          <div>Content Description</div>
-        </div>
-      </div>
+  const [contentColor, setContentColor] = useState(false);
+
+  const HandleMenu = (str)=>{
+    //capture what happened in the menu and change the page state based on it
+    // alert(str);
+    if(str === "bg"){
+      setContentColor("#DCA877");
+    }
+  }
+  
+  
+  return <div className="home">
+    <div className="content">
+      <Content bgcolor={contentColor}>
+        <h1>Welcome to my app</h1>
+        <div>Hi</div>
+      </Content>
     </div>
-  )
+    <div className="menu">
+      <Menu onMenuSelect={HandleMenu}/>
+    </div>
+  </div>
 }
